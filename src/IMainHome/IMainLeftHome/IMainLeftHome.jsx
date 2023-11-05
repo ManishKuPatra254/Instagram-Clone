bf9081ec04cbea405ae5e1d2da07f4142a459339
ManishKuPatra254 <manishpatra254@gmail.com>
2023-11-05 11:43:26 +0530

    changed width to %

--- src/IMainHome/IMainLeftHome/IMainLeftHome.jsx
+++ src/IMainHome/IMainLeftHome/IMainLeftHome.jsx
@@ -1,11 +1,12 @@
 import { Fragment } from 'react';
 import styles from './IMainLeftHome.module.css';
+import instalogo from '../../assets/insta 1.png'
 
 export function IMainLeftHome() {
     return (
         <Fragment>
             <div className={styles.main_left_home}>
-                
+                <img src={instalogo} alt="" />
             </div>
         </Fragment>
     )
